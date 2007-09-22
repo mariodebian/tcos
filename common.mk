963eb480c73d436c4ee3e67d09f6bc84d7174deb
Mario Izquierdo (mariodebian) <mariodebian@gmail.com>
2007-09-22 17:10:11 +0000

    initramfs-tools-tcos (0.84.2)   * bin/tcos-buildroot:     - Fix typo in TCOS_CHROOT var     - Add --gentcos option to build images with other params   * Update Ubuntu gutsy kernel version   * debian/control:     - Replace source:version depends with 0.84.1     - Fix some descriptions
    git-svn-id: http://tcosproject.org/svn/tcos/trunk/initramfs-tools-tcos@474 97557d6f-af72-4ac7-98cc-18709f36555f

--- common.mk
+++ common.mk
@@ -23,7 +23,7 @@ USPLASH_unstable=0.4.1
 KERNEL_experimental="2.6.22-2-486"
 USPLASH_experimental=0.4.1
 
-KERNEL_gutsy="2.6.22-10-generic"
+KERNEL_gutsy="2.6.22-12-generic"
 USPLASH_gutsy=0.4.1
 ##################################
 
