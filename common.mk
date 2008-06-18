82c239b2a5d78cf2ecd54b145bd54b4affcbf874
Mario Izquierdo (mariodebian) <mariodebian@gmail.com>
2008-06-18 16:06:55 +0000

    initramfs-tools-tcos (0.89~rc9)   * bin/tcos-buildchroot:     - Add standalone xauth package to new distros     - Copy console keymap   * hooks-addons/25tcosmonitor     - Delete start-stop-daemon of image before copying   * common.mk     - Update Ubuntu hardy and MaX4.0 kernel version   * hooks/tcosmods     - Search sound/isa patch, don't use debian default path
    git-svn-id: http://tcosproject.org/svn/tcos/trunk/initramfs-tools-tcos@871 97557d6f-af72-4ac7-98cc-18709f36555f

--- common.mk
+++ common.mk
@@ -29,10 +29,10 @@ USPLASH_experimental=0.4.1
 KERNEL_gutsy="2.6.22-14-generic"
 USPLASH_gutsy=0.4.1
 
-KERNEL_max="2.6.24-17-generic"
+KERNEL_max="2.6.24-19-generic"
 USPLASH_max=0.4.1
 
-KERNEL_hardy="2.6.24-17-generic"
+KERNEL_hardy="2.6.24-19-generic"
 USPLASH_hardy=0.4.1
 ##################################
 
