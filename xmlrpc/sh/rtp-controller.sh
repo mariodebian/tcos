a6b18022e0ab28d5a40ea533d264f4939592add1
Ignacio Vidal <vidal_joshur@gva.es>
2008-09-23 17:10:03 +0000

    initramfs-tools-tcos (0.89.9)   * debian/control:     - Update depends   * scripts/tcos-bottom/40initsound xmlrpc/sh/rtp-controller.sh     hooks-addons/21pulseaudio:     -  Update scripts to allow new chat mode
    git-svn-id: http://tcosproject.org/svn/tcos/trunk/initramfs-tools-tcos@992 97557d6f-af72-4ac7-98cc-18709f36555f

--- xmlrpc/sh/rtp-controller.sh
+++ xmlrpc/sh/rtp-controller.sh
@@ -36,23 +36,7 @@ if [ -e /conf/tcos-run-functions ]; then
       ln -s $lib $(echo $lib | awk -F"\.so" '{print $1".so.0"}')  >> /tmp/sox.libs 2>&1
     done
   fi
-
-else
-  STANDALONE=1
-  STANDALONE_USER=$(w | awk '{ if ($3 == ":0" || $2 == ":0") print $1 }' |head -1)
-  STANDALONE_USER=$(/usr/lib/tcos/tcos-last --user 2>/dev/null)
-  if [ "${STANDALONE_USER}" = "" ]; then echo "error: no standalone user connected"; exit 1; fi
-  DBUS_HANDLER="/usr/lib/tcos/tcos-dbus-helper --username=${STANDALONE_USER} "
-fi
-
-
-
-for arg in $1; do
-  case $arg in
-     startrtp)
-        mkfifo /tmp/audiofifo 2>/dev/null
-        if [ $STANDALONE = 0 ]; then
-             cat << EOF > /etc/asound.conf
+  cat << EOF > /etc/asound.conf
 pcm.!default {
     type pulse
 }
@@ -60,25 +44,90 @@ ctl.!default {
     type pulse
 }
 EOF
-             /sbin/daemonize.sh "rtpdump" "-F payload -o /tmp/audiofifo $2/1234"
-             /sbin/daemonize.sh "play" "-t raw -r 44100 -s -2 -c 2 -B /tmp/audiofifo"
-             if [ $? = 0 ]; then echo "ok"; else echo "error: starting rtp play"; fi
+  DAEMONIZE="/sbin/daemonize.sh"
+else
+  STANDALONE=1
+  STANDALONE_USER=$(w | awk '{ if ($3 == ":0" || $2 == ":0") print $1 }' |head -1)
+  STANDALONE_USER=$(/usr/lib/tcos/tcos-last --user 2>/dev/null)
+  if [ "${STANDALONE_USER}" = "" ]; then echo "error: no standalone user connected"; exit 1; fi
+  DBUS_HANDLER="/usr/lib/tcos/tcos-dbus-helper --username=${STANDALONE_USER} "
+  DAEMONIZE="/usr/lib/tcos/daemonize.sh"
+fi
+
+version=$(pactl --version | awk '{print $2}' | awk -F"." '{if (($2 >= 9) && ($3 >= 10)) printf "yes"}')
+
+for arg in $1; do
+  case $arg in
+     startrtp-recv)
+        if [ ! -z $version ]; then
+            $DAEMONIZE "pactl" "load-module module-rtp-recv sap_address=$2"
+            if [ $? = 0 ]; then echo "ok"; else echo "error: starting pulse recv module"; fi
         else
-             /usr/lib/tcos/daemonize.sh "rtpdump" "-F payload -o /tmp/audiofifo $2/1234"
-             $DBUS_HANDLER --auth=$3 --type=exec --text="play -t raw -r 44100 -s -2 -c 2 -B /tmp/audiofifo"
+            mkfifo /tmp/audiofifo 2>/dev/null
+            if [ $STANDALONE = 0 ]; then
+               $DAEMONIZE "rtpdump" "-F payload -o /tmp/audiofifo $2/1234"
+               $DAEMONIZE "play" "-t raw -r 11025 -s -2 -c 2 -B /tmp/audiofifo"
+               if [ $? = 0 ]; then echo "ok"; else echo "error: starting rtp play"; fi
+            else
+               $DAEMONIZE "rtpdump" "-F payload -o /tmp/audiofifo $2/1234"
+               $DBUS_HANDLER --auth=$3 --type=exec --text="play -t raw -r 11025 -s -2 -c 2 -B /tmp/audiofifo"
+            fi
         fi
      ;;
-     stoprtp)
-        if [ $STANDALONE = 0 ]; then
-            killall -SIGKILL play 2>/dev/null
-            killall -SIGKILL rtpdump 2>/dev/null
-            rm -f /tmp/audiofifo /etc/asound.conf
-            if [ $? = 0 ]; then echo "ok"; else echo "error: stopping rtp play"; fi
+     stoprtp-recv)
+        if [ ! -z $version ]; then
+           index=$(pactl list | grep -B1 "module-rtp-recv" | head -1 | awk '{printf $3}' | sed 's/#//g')
+           $DAEMONIZE "pactl" "unload-module $index"
+            if [ $? = 0 ]; then echo "ok"; else echo "error: stopping pulse recv module"; fi
         else
-            $DBUS_HANDLER --auth=$2 --type=exec --text="killall -s KILL play" 2>/dev/null
-            killall -s KILL rtpdump 2>/dev/null
-            rm -f /tmp/audiofifo
-            if [ $? = 0 ]; then echo "ok"; else echo "error: stopping rtp play"; fi
+           if [ $STANDALONE = 0 ]; then
+              killall -SIGKILL play 2>/dev/null
+              killall -SIGKILL rtpdump 2>/dev/null
+              rm -f /tmp/audiofifo /etc/asound.conf
+              if [ $? = 0 ]; then echo "ok"; else echo "error: stopping rtp play"; fi
+           else
+              $DBUS_HANDLER --auth=$2 --type=exec --text="killall -s KILL play" 2>/dev/null
+              killall -s KILL rtpdump 2>/dev/null
+              rm -f /tmp/audiofifo
+              if [ $? = 0 ]; then echo "ok"; else echo "error: stopping rtp play"; fi
+           fi
+        fi
+     ;;
+     startrtp-send)
+        if [ ! -z $version ]; then
+            $DAEMONIZE "pactl" "load-module module-rtp-send destination=$2 rate=11025 channels=2 port=1234"
+            if [ $? = 0 ]; then echo "ok"; else echo "error: starting pulse send module"; fi
+        else
+            echo "error: starting pulse send module, incorrect pulseaudio version"
+        fi
+     ;;
+     stoprtp-send)
+        if [ ! -z $version ]; then
+           index=$(pactl list | grep -B1 "module-rtp-send" | head -1 | awk '{printf $3}' | sed 's/#//g')
+           $DAEMONIZE "pactl" "unload-module $index"
+            if [ $? = 0 ]; then echo "ok"; else echo "error: stopping pulse send module"; fi
+        else
+            echo "error: stopping pulse send module, incorrect pulseaudio version"
+        fi
+     ;;
+     startrtp-chat)
+        if [ ! -z $version ]; then
+            $DAEMONIZE "pactl" "load-module module-rtp-send destination=$2 rate=11025 channels=2 port=1234"
+            $DAEMONIZE "pactl" "load-module module-rtp-recv sap_address=$2"
+            if [ $? = 0 ]; then echo "ok"; else echo "error: starting pulse chat mode"; fi
+        else
+            echo "error: starting pulse chat mode, incorrect pulseaudio version"
+        fi
+     ;;
+     stoprtp-chat)
+        if [ ! -z $version ]; then
+           index_send=$(pactl list | grep -B1 "module-rtp-send" | head -1 | awk '{printf $3}' | sed 's/#//g')
+           index_recv=$(pactl list | grep -B1 "module-rtp-recv" | head -1 | awk '{printf $3}' | sed 's/#//g')
+           $DAEMONIZE "pactl" "unload-module $index_send"
+           $DAEMONIZE "pactl" "unload-module $index_recv"
+            if [ $? = 0 ]; then echo "ok"; else echo "error: stopping pulse chat mode"; fi
+        else
+            echo "error: stopping pulse chat mode, incorrect pulseaudio version"
         fi
      ;;
      *)
