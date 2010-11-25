acfe90b50062579ec17fc35924a2d6935127f265
Mario Izquierdo (mariodebian) <mariodebian@gmail.com>
2010-11-25 13:51:32 +0100

    common.mk Update Ubuntu Lucid kernel 2.6.32-26

--- common.mk
+++ common.mk
@@ -17,8 +17,8 @@ KERNEL_jaunty="2.6.28-19-generic"
 
 KERNEL_karmic="2.6.31-22-generic"
 
-KERNEL_lucid="2.6.32-25-generic"
-KERNEL_max="2.6.32-25-generic"
+KERNEL_lucid="2.6.32-26-generic"
+KERNEL_max="2.6.32-26-generic"
 
 KERNEL_maverick="2.6.35-22-generic"
 ##################################
